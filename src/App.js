2a406a0b15a04f3d3af98dea84fd146bdcb88616
Cloud User <ec2-user@ip-172-31-33-112.us-east-2.compute.internal>
2019-01-16 21:33:49 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -9,15 +9,23 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            hi baby 
+          </p>
+          <p>
+            i love you so much 
+          </p>
+          <p>
+            you are the best  
           </p>
           <a
             className="App-link"
-            href="https://reactjs.org"
+            href="https://reactsjs.org"
             target="_blank"
             rel="noopener noreferrer"
-          >
+           >
+            
             Learn React
+             
           </a>
         </header>
       </div>
